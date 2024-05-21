12be0f8d5fdd8d721bbaffc1dc2d78a40aa51f6b
KunJon-analytics <johnadelakun1@gmail.com>
2024-05-21 10:56:27 +0100

    moved play now main button to active game button component

--- components/home/twitter-link.tsx
+++ components/home/twitter-link.tsx
@@ -2,22 +2,13 @@
 
 import React from "react";
 import { useUtils } from "@tma.js/sdk-react";
-import { useRouter } from "next/navigation";
-
 import { siteConfig } from "@/config/site";
 
 import { Button } from "../ui/button";
 import { Icons } from "../shared/icons";
-import MainButtonManipulator from "../twa/main-button-manipulator";
-
-type TwitterLinkProps = {};
 
 const TwitterLink = () => {
   const utils = useUtils(true);
-  const { push } = useRouter();
-  const action = () => {
-    push("/dashboard");
-  };
 
   return (
     <>
@@ -31,7 +22,6 @@ const TwitterLink = () => {
       >
         Introducing on <Icons.twitter className="ml-2 h-4 w-4" />
       </Button>
-      <MainButtonManipulator action={action} buttonText="View Points" showMb />
     </>
   );
 };
--- components/shared/active-game-button.tsx
+++ components/shared/active-game-button.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import Link from "next/link";
+import { useMainButton } from "@tma.js/sdk-react";
+import { useRouter } from "next/navigation";
 
 import useActiveGame from "@/hooks/use-active-game";
 
@@ -10,6 +12,26 @@ import LoadingButton from "./loading-button";
 
 const ActiveGameButton = () => {
   const { activeGame, isLoading } = useActiveGame();
+  const router = useRouter();
+  const mainButton = useMainButton(true);
+
+  const goToGame = useCallback(() => {
+    if (activeGame && mainButton?.isVisible) {
+      router.push(`/play/${activeGame.id}`);
+      mainButton.hide();
+    }
+  }, [activeGame, mainButton]);
+
+  useEffect(() => {
+    if (!activeGame) {
+      mainButton?.hide();
+    } else {
+      mainButton?.show();
+      mainButton?.setText("Play Now!!!");
+    }
+  }, [activeGame, mainButton]);
+
+  useEffect(() => mainButton?.on("click", goToGame), [mainButton, goToGame]);
 
   if (isLoading) {
     return <LoadingButton />;
