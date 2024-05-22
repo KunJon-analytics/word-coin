b7f8a6a02f975aa44007b2f4293600580b27ba7f
KunJon-analytics <johnadelakun1@gmail.com>
2024-05-22 12:33:53 +0100

    added main button to about

--- app/(marketing)/about/layout.tsx
+++ app/(marketing)/about/layout.tsx
@@ -1,8 +1,36 @@
+import { cn } from "@/lib/utils";
+
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="p-4 prose prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-black prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg dark:prose-headings:text-white">
-      {children}
-    </div>
+    <article className="mx-auto max-w-xl px-6 py-20">
+      {/* <div className="text-center">
+          <time dateTime={date} className="mb-1 text-xs text-gray-600">
+            {format(parseISO(date), "LLLL d, yyyy")}
+          </time>
+          <p className="text-sm font-semibold">by {author}</p>
+        </div> */}
+      <div className="mt-12">
+        <div
+          className={cn(
+            "prose prose-slate max-w-none dark:prose-invert dark:text-slate-400",
+            // headings
+            "prose-headings:scroll-mt-28 prose-headings:font-cal prose-headings:font-semibold lg:prose-headings:scroll-mt-[8.5rem]",
+            // lead
+            "prose-lead:text-slate-500 dark:prose-lead:text-slate-400",
+            // links
+            "prose-a:font-semibold dark:prose-a:text-blue-400",
+            // link underline
+            "prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.blue.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.blue.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px]",
+            // pre
+            "prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10",
+            // hr
+            "dark:prose-hr:border-slate-800"
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    </article>
   );
 }
--- components/twa/main-button-manipulator.tsx
+++ components/twa/main-button-manipulator.tsx
@@ -10,7 +10,7 @@ const MainButtonManipulator = () => {
   const mb = useMainButton(true);
 
   useEffect(() => {
-    const routes = ["/"];
+    const routes = ["/", "/about"];
 
     if (routes.includes(pathname)) {
       mb?.show();
