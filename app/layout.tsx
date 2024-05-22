422d09d3923327e12bbb5a0be1b0320f3d04130e
KunJon-analytics <johnadelakun1@gmail.com>
2024-05-22 11:12:34 +0100

    added script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
