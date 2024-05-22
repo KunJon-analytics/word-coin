97453b1367790cbe1d2698f008f201eb6c6fdf06
KunJon-analytics <johnadelakun1@gmail.com>
2024-05-22 04:39:04 +0100

    chnaged muted to muted foreground

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,6 @@
 import { marketingConfig } from "@/config/marketing";
 import { NavBar } from "@/components/layout/navbar";
 import { SiteFooter } from "@/components/layout/site-footer";
-import { SquaresPattern } from "@/components/shared/squares-pattern";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -13,7 +12,7 @@ export default async function MarketingLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar items={marketingConfig.mainNav} scroll={true} />
-      <SquaresPattern />
+
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
